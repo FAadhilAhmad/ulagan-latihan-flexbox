25a37a0cd18c5321b8498546d807f51f18787f3e
FAadhilAhmad <fadhilsyawaludin5@gmail.com>
2025-02-16 21:11:46 +0700

    Update data.js
    belajar aritmatika meski ga fungsi cuakss

--- data.js
+++ data.js
@@ -1,2 +1,21 @@
 alert('hello fadhil permulaan yg bagus')
 
+// latihan aritmatika 
+let angka1 = prompt(1);
+let angka2 = prompt(2);
+
+// konversi input string menjadi angka 
+angka1 = Number(10);
+angka2 = Number(2);
+
+// melakukan operasi matematika
+let penjumlahan = angka1 + angka2;
+let pengurangan = angka1 - angka2;
+let perkalian = angka1 * angka2;
+let pembagian = angka1 / angka2;
+
+// menampilkan hasil di console 
+console.log("hasil pemjumlahan:" + penjumlahan)
+console.log("hasil pengurangan:" + pengurangan)
+console.log("hasil perkalian:" + perkalian)
+console.log("hasil pembagian" + pembagian)
